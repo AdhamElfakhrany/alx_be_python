ef1aed2326eded74896d5adfa5fa86f5abe99dd8
adham elfakhrany <54482105+AdhamElfakhrany@users.noreply.github.com>
2025-08-04 19:18:33 +0300

    Update simple_calculator.py

--- programming_paradigm /simple_calculator.py
+++ programming_paradigm /simple_calculator.py
@@ -1,6 +1,4 @@
 class SimpleCalculator:
-    """A simple calculator class that supports basic arithmetic operations."""
-
     def add(self, a, b):
         """Return the addition of a and b."""
         return a + b
