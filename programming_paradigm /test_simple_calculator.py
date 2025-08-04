5106b3eb04df6ea25d1bec11a4d67516eeaa1dd9
adham elfakhrany <54482105+AdhamElfakhrany@users.noreply.github.com>
2025-08-04 19:10:12 +0300

    Update test_simple_calculator.py

--- programming_paradigm /test_simple_calculator.py
+++ programming_paradigm /test_simple_calculator.py
@@ -1,26 +1,36 @@
-
 import unittest
 from simple_calculator import SimpleCalculator
 
-class TestSimpleCalculator(unittest.TestCase):
+class TestCalc(unittest.TestCase):
+    """
+    Test Cases for the Simple Calculator.
+    """
     def setUp(self):
         self.calc = SimpleCalculator()
-
-    def test_add(self):
-        self.assertEqual(self.calc.add(2, 3), 5)
-        self.assertEqual(self.calc.add(-1, -1), -2)
-
-    def test_subtract(self):
-        self.assertEqual(self.calc.subtract(10, 5), 5)
-        self.assertEqual(self.calc.subtract(0, 3), -3)
-
-    def test_multiply(self):
-        self.assertEqual(self.calc.multiply(2, 4), 8)
-        self.assertEqual(self.calc.multiply(-2, 3), -6)
-
-    def test_divide(self):
-        self.assertEqual(self.calc.divide(10, 2), 5.0)
-        self.assertIsNone(self.calc.divide(5, 0))
+    
+    def test_addition(self):
+        self.assertEqual(self.calc.add(5,5), 10)
+        self.assertEqual(self.calc.add(2,3), 5)
+        self.assertEqual(self.calc.add(-1,1), 0)
+    
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5,2), 3)
+        self.assertEqual(self.calc.subtract(3,2), 1)
+        self.assertEqual(self.calc.subtract(-1,1), -2)
+    
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(5,2), 10)
+        self.assertEqual(self.calc.multiply(4,3), 12)
+        self.assertEqual(self.calc.multiply(1,2), 2)
+    
+    def test_division(self):
+        self.assertEqual(self.calc.divide(5,5), 1)
+        self.assertIsNone(self.calc.divide(10,0))
 
 if __name__ == '__main__':
     unittest.main()
+
+# if the main fuction Raised Zero Divison Error we do this:
+    # with self.assertRaises(ZeroDivisionError):
+    #     SimpleCalculator().divide(10,0)
+    # or assert.assertraises(ZeroDivisionError,SimpleCalculator().divide, 10,0)
