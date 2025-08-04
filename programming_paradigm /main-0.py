3efc821bbfad92a03d16805f623e72f0f5202018
adham elfakhrany <54482105+AdhamElfakhrany@users.noreply.github.com>
2025-08-04 18:55:11 +0300

    Update main-0.py

--- programming_paradigm /main-0.py
+++ programming_paradigm /main-0.py
@@ -2,20 +2,25 @@ import sys
 from bank_account import BankAccount
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("Usage: main-0.py <owner> <action> <amount>")
+    if len(sys.argv) < 3:
+        print("Usage: main-0.py <owner> <action> [<amount>]")
         sys.exit(1)
 
     owner = sys.argv[1]
     action = sys.argv[2]
-    amount = float(sys.argv[3])
 
     account = BankAccount(owner)
 
     if action == "deposit":
-        account.deposit(amount)
+        if len(sys.argv) != 4:
+            print("Amount required for deposit.")
+        else:
+            account.deposit(float(sys.argv[3]))
     elif action == "withdraw":
-        account.withdraw(amount)
+        if len(sys.argv) != 4:
+            print("Amount required for withdraw.")
+        else:
+            account.withdraw(float(sys.argv[3]))
     elif action == "display":
         account.display_balance()
     else:
